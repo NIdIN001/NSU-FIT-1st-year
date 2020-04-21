85adfaf051538b1286c0fd45e97dab8973add441
Stanislav <62795541+NIdIN001@users.noreply.github.com>
2020-04-22 01:58:09 +0700

    Update functions.c

--- 8.0 kruskal algorithm/functions.c
+++ 8.0 kruskal algorithm/functions.c
@@ -4,7 +4,7 @@ int compare(const void* a, const void* b) {
     return ((((Tnode *) a)->leight) - (((Tnode *) b)->leight));
 }
 
-struct frame frame(Tnode edges[],Tnode out[], int lines, int vertex) {
+struct result frame(Tnode edges[],Tnode out[], int lines, int vertex) {
 
     qsort(edges, lines, sizeof(Tnode), compare);
 
@@ -16,26 +16,23 @@ struct frame frame(Tnode edges[],Tnode out[], int lines, int vertex) {
     int count = 0;
 
     for (int i = 0; i < lines; i++) {
-        if (color[edges[i].fp-1] != color[edges[i].sp-1]) {
+        if (color[edges[i].fp - 1] != color[edges[i].sp - 1]) {
             out[count] = edges[i];
             count++;
             for (int j = 0; j < vertex; j++) {
-                if (color[j] == color[edges[i].sp-1]) {
-                    color[j] = color[edges[i].fp-1];
+                if (color[j] == color[edges[i].sp - 1]) {
+                    color[j] = color[edges[i].fp - 1];
                 }
             }
         }
     }
     int is_ok = 0;
-    for (int i = 0; i < vertex - 1; i++) {
-        for (int j = i; j < vertex; j++) {
-            if (color[i] != color[j]) {
-                is_ok++;
-            }
-        }
+    for (int i = 1; i < vertex; i++) {
+        if (color[0] != color[i])
+            is_ok++;
     }
-    struct frame ret;
-    ret.count = count;
-    ret.is_ok = is_ok;
-    return ret;
+    struct result result;
+    result.count = count;
+    result.is_ok = is_ok;
+    return result;
 }
