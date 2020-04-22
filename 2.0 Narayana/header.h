1ece89bc92843ffaa1d674ffb9083f91d2e1369c
Stanislav <62795541+NIdIN001@users.noreply.github.com>
2020-04-22 13:16:18 +0700

    Add files via upload

---
+++ 2.0 Narayana/header.h
@@ -0,0 +1,13 @@
+//
+// Created by Станислав Уточкин on 28.03.2020.
+//
+
+#ifndef LABA2_HEADER_H
+#define LABA2_HEADER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void swap(int *x, int *y);
+void bad_input_check(int* arr, int size);
+void Narayana(int* arr, size_t size);
+#endif //LABA2_HEADER_H
