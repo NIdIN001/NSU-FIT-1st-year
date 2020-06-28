9eb831b47528535562eb87fde49ac9947ffeeb7f
Stanislav <62795541+NIdIN001@users.noreply.github.com>
2020-06-28 23:47:50 +0700

    Update DFS.c

--- 7.0 topological sorting/DFS.c
+++ 7.0 topological sorting/DFS.c
@@ -1,20 +1,70 @@
 #include "header.h"
 
-void DFS(int x,int* visited,int vertex, int array[][vertex]) { //обход графа в глубину
-    if (visited[x] == 2) {
-        return;
+
+void initializeVector(Vector* vector, int countOfVertex) {
+    for (int i = 0; i < countOfVertex; i++) {
+        vector[i].capacity = 10;
+        vector[i].size = 0;
+        vector[i].data = (int*)calloc(10, sizeof(int));
     }
-    if (visited[x] == 1) {
-        printf("impossible to sort\n");
-        exit(EXIT_SUCCESS);
-    }
-    visited[x] = 1;
-//написать рекурсивную часть функции DFS
-    for (int i = 0; i < vertex; i++) {
-        if (array[i][x] == 1) {
-            DFS(i, visited, vertex, array);
-        }
-    }
-    visited[x] = 2;
-    push(x);
 }
+
+void pushInVector(Vector* vector, int vertex) {
+    if (vector->capacity < vector->size + 1) {
+        vector->capacity = vector->capacity + 10;
+        vector->data = (int*)realloc(vector->data, sizeof(int) * vector->capacity);
+    }
+    vector->data[vector->size] = vertex;
+    vector->size++;
+}
+
+
+void sort(Vector* vector, int index, int* color_table, int* output, int* sizeOutput) {
+    color_table[index] = 1;
+
+    for (int i = 0; i < vector[index].size; i++) {
+        if (color_table[vector[index].data[i]] == 2)
+            break;
+        if (color_table[vector[index].data[i]] == 1) {
+            printf("impossible to sort");
+            exit(EXIT_SUCCESS);
+        }
+
+        if (color_table[vector[index].data[i]] == 0)
+            sort(vector, vector[index].data[i], color_table, output, sizeOutput);
+
+
+    }
+
+    color_table[index] = 2;
+    output[*sizeOutput] = index + 1;
+    *sizeOutput = *sizeOutput + 1;
+}
+
+void sortGraph(Vector* vector, int countOfVertex, int starterVertex, int* output) {
+    int* color_table;
+    color_table = (int*)calloc(countOfVertex, sizeof(int));
+
+    int sizeOutput = 0;
+    sort(vector, starterVertex, color_table, output, &sizeOutput);
+
+    for (int i = 0; i < countOfVertex; i++) {
+        if (color_table[i] == 0 && i != starterVertex && color_table[i] != 2)
+            sort(vector, i, color_table, output, &sizeOutput);
+    }
+
+    free(color_table);
+}
+
+void printGraph(int* output, int countOfVertex) {
+    for (int i = countOfVertex - 1; i >= 0; i--)
+        printf("%d ", output[i]);
+}
+
+void removeVector(Vector** vector, int countOfVertex) {
+    for (int i = 0; i < countOfVertex; i++)
+        free((*vector)[i].data);
+
+    free(*vector);
+}
+
