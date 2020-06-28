f56cb92870e7d4e02318a2e09333ffb5de7168b1
Stanislav <62795541+NIdIN001@users.noreply.github.com>
2020-06-28 23:45:07 +0700

    Update header.h

--- 5.0 huffman/header.h
+++ 5.0 huffman/header.h
@@ -1,34 +1,29 @@
-//
-// Created by Станислав Уточкин on 06.05.2020.
-//
+#ifndef LAB5_HEADER_H
+#define LAB5_HEADER_H
 
-#ifndef LABA5_HEADER_H
-#define LABA5_HEADER_H
-
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define SIZE_OUT 1024*700
+typedef struct Node {
+    size_t freq;
+    int symbol;
+    struct Node *right;
+    struct Node *left;
+} Tnode;
 
-typedef struct node {
-    unsigned int weight;
-    unsigned int value;
-    struct node *left;
-    struct node *right;
-}Tnode;
+#define MEGABUF_SIZE 4096
+#define OUTBUF_SIZE 1024 * 700
 
+Tnode *newNode(Tnode *left, Tnode *right, size_t freq, int symbol);
+void coder(unsigned char *inpbuf, unsigned int bitpos, Tnode *root, size_t file_length, FILE *output);
+Tnode *read_tree(unsigned char *outbuf, unsigned int *total_bitpos);
+Tnode *read_header(unsigned char *outbuf, unsigned int *bitpos, unsigned int *file_length);
+void decode(FILE *input, FILE *output);
+void decoder(FILE *input, FILE *output, unsigned int *total_bitpos, unsigned char *outbuf, unsigned char *code[256]);
+void make_huff_table(unsigned char path[256], unsigned int level, unsigned char *code[256], Tnode *root);
+void save_tree(Tnode *root, unsigned int *total_bitpos, unsigned char *outbuf);
+Tnode *build_tree(unsigned int *letters);
+void encode(FILE *input, FILE *output);
 
-size_t make_signs(FILE* input, int* letters_count);
-void delete_tree(Tnode* root);
-Tnode* make_huff_tree(int* signs);
-void save_huffman_tree(Tnode* root, unsigned int* total_bitpos, unsigned char* outbuf);
-void make_huff_table(Tnode *root, unsigned char *code[256], unsigned char path[256], unsigned int level);
-void write_encode(FILE *input, FILE *output, unsigned int *total_bitpos, unsigned char *outbuf, unsigned char *code[256]);
-void code(FILE *input ,FILE *output);
-Tnode *read_huffman_tree(unsigned char *outbuf, size_t *total_bitpos);
-size_t read_header(unsigned char *outbuf, size_t *bitpos, size_t message_len);
-void write_raw(unsigned char *inpbuf, size_t bitpos, Tnode *root, size_t message_len, FILE *output);
-void decode(FILE *input ,FILE *output);
-
-#endif //LABA5_HEADER_H
+#endif //LAB5_HEADER_H
