52529297b3c5aecf8d2c8b1d601b7def218cdfe7
Stanislav <62795541+NIdIN001@users.noreply.github.com>
2020-06-28 23:44:44 +0700

    Update huffman.c

--- 5.0 huffman/huffman.c
+++ 5.0 huffman/huffman.c
@@ -1,233 +1,226 @@
-//
-// Created by Станислав Уточкин on 06.05.2020.
-//
 #include "header.h"
 
-size_t make_signs(FILE* input, int* letters_count) {
-    int c;
-    size_t text_len = 0;
-    while (!feof(input)) {
-        c = fgetc(input);
-        if (c == -1)
-            continue;
-        letters_count[c]++;
-        text_len++;
-    }
-    return text_len;
+Tnode *newNode(Tnode *left, Tnode *right, size_t freq, int symbol) {
+    Tnode *new = (Tnode *) malloc(sizeof(Tnode));
+    new->freq = freq;
+    new->symbol = symbol;
+    new->left = left;
+    new->right = right;
+    return new;
 }
 
-void delete_tree(Tnode* root) {
-    if (root->left != NULL) {
-        delete_tree(root->left);
-    }
-    if (root->right != NULL) {
-        delete_tree(root->right);
-    }
-    if ((root->left == NULL) && (root->right == NULL))
-        free(root);
-}
+void coder(unsigned char *inpbuf, unsigned int bitpos, Tnode *root, size_t file_length, FILE *output) {
+    unsigned char outbuf[MEGABUF_SIZE];
+    size_t k;
+    size_t outpos = 0;
+    size_t pos = bitpos / 8;
+    size_t bit_pos = bitpos % 8;
 
-Tnode* make_huff_tree(int* signs) {
-    Tnode *array[256] = {NULL};
-    int count = 0;
+    for (k = 0; k < file_length; k++) {
+        Tnode *node = root;
 
-    for (size_t i = 0; i < 256; i++) {
-        if (signs[i] > 0) {
-            Tnode *new = (Tnode *) malloc(sizeof(Tnode));
-            new->value = i;
-            new->weight = signs[i];
-            new->left = NULL;
-            new->right = NULL;
-            array[count] = new;
-            count++;
-        }
-    }//построил элементы графа с ненулевой встречаймостью в коде
+        while (node->left != NULL) {
 
-    while (count > 1) {
-        for (int i = 0; i < count - 1; i++) {
-            for (int j = 0; j < count - i - 1; j++) {
-                if (array[j]->weight < array[j + 1]->weight) {
-                    Tnode *tmp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = tmp;
-                }
+            node = (inpbuf[pos] & (128u >> bit_pos)) ? node->right : node->left;
+
+            if (8 == ++bit_pos) {
+                bit_pos = 0;
+                pos++;
             }
         }
-        Tnode *new = (Tnode *) malloc(sizeof(Tnode));
-        new->left = array[count - 2];
-        new->right = array[count - 1];
-        new->value = 0;
-        new->weight = array[count - 1]->weight + array[count - 2]->weight;
-        array[count - 2] = new;
-        count--;
+        outbuf[outpos++] = (unsigned char) node->symbol;
+
+        if (MEGABUF_SIZE == outpos) {
+            size_t written = fwrite(outbuf, 1, outpos, output);
+
+            if (outpos != written) {
+                printf("File write error\n");
+                exit(1);
+            }
+
+            outpos = 0;
+        }
     }
-    return array[0];
+    if (outpos > 0) {
+        size_t written = fwrite(outbuf, 1, outpos, output);
+
+        if (outpos != written) {
+            printf("File write error\n");
+            exit(1);
+        }
+    }
 }
 
-void save_huffman_tree(Tnode* root, unsigned int* total_bitpos, unsigned char* outbuf) {
-    if (root->left) {
-        outbuf[*total_bitpos / 8] |= 128u >> ((*total_bitpos) % 8);
-        *total_bitpos += 1;
+Tnode *read_tree(unsigned char *outbuf, unsigned int *total_bitpos) {
+    /* выделяем память для одного узла дерева */
+    Tnode *nt = (Tnode *) malloc(sizeof(Tnode));
+    int bit = outbuf[*total_bitpos / 8] & (128u >> (*total_bitpos % 8));
+    (*total_bitpos)++;
 
-        save_huffman_tree(root->left, total_bitpos, outbuf);
-        save_huffman_tree(root->right, total_bitpos, outbuf);
+    if (bit) {
+        /* узел не является листом */
+        nt->left = read_tree(outbuf, total_bitpos);
+        nt->right = read_tree(outbuf, total_bitpos);
     } else {
-        unsigned int bitpos;
-        *total_bitpos += 1;
-        bitpos = (*total_bitpos) % 8;
+        unsigned int bitpos = *total_bitpos % 8;
+        /* иначе узел root является листом */
+        nt->left = NULL;
+        nt->right = NULL;
+        /* считываем из файла код символа */
         if (bitpos == 0) {
-            outbuf[*total_bitpos / 8] = root->value;
+            nt->symbol = outbuf[*total_bitpos / 8];
         } else {
-            outbuf[*total_bitpos / 8] |= root->value >> bitpos;
-            outbuf[*total_bitpos / 8 + 1] |= root->value << (8 - bitpos);
+            nt->symbol = (unsigned char) (outbuf[*total_bitpos / 8] << bitpos);
+            nt->symbol |= outbuf[*total_bitpos / 8 + 1] >> (8 - bitpos);
         }
         *total_bitpos += 8;
     }
+    return nt;
 }
 
-void make_huff_table(Tnode *root, unsigned char *code[256], unsigned char path[256], unsigned int level) {
-    if (root->value == 0) {
-        if (root->left != NULL) {
-            path[level] = '0';
-            make_huff_table(root->left, code, path, level + 1);
-        }
-        if (root->right != NULL) {
-            path[level] = '1';
-            make_huff_table(root->right, code, path, level + 1);
-        }
-    } else {
-        code[root->value] = (unsigned char *) malloc(sizeof(unsigned char) * level);
-        memcpy(code[root->value], path, level);
-    }
+Tnode *read_header(unsigned char *outbuf, unsigned int *bitpos, unsigned int *file_length) {
+    *file_length = outbuf[0] << 24;
+    *file_length |= outbuf[1] << 16;
+    *file_length |= outbuf[2] << 8;
+    *file_length |= outbuf[3];
+    *bitpos = 32;
+    if (*file_length > 0)
+        return read_tree(outbuf, bitpos);
+    return NULL;
 }
 
-void write_encode(FILE *input, FILE *output, unsigned int *total_bitpos, unsigned char *outbuf, unsigned char *code[256]) {
+void decode(FILE *input, FILE *output) {
+    unsigned char *outbuf = (unsigned char *) malloc(OUTBUF_SIZE);
+    unsigned int bitpos = 0;
+    unsigned int message_len = fread(outbuf, 1, OUTBUF_SIZE, input);
+
+    if (message_len == 0)
+        exit(0);
+    Tnode *root = read_header(outbuf, &bitpos, &message_len);
+    coder(outbuf, bitpos, root, message_len, output);
+}
+
+void decoder(FILE *input, FILE *output, unsigned int *total_bitpos, unsigned char *outbuf, unsigned char *code[256]) {
+    unsigned char inpbuf[MEGABUF_SIZE] = {0};
     unsigned int bytepos = *total_bitpos / 8;
     unsigned int bitpos = *total_bitpos % 8;
-    int c;
+    size_t frag = 0;
 
-    while (!feof(input)) {
-        c = fgetc(input);
-        if (c == -1)
-            continue;
-        unsigned char *current_code = code[c];
+    while ((frag = fread(inpbuf, 1, MEGABUF_SIZE, input)) > 0) {
+        for (size_t i = 0; i < frag; i++) {
+            unsigned char *current_code = code[inpbuf[i]];
 
-        for (unsigned int j = 0; current_code[j] != 0; j++) {
-            if (current_code[j] == '1')
-                outbuf[bytepos] |= 128u >> bitpos;
+            for (unsigned int j = 0; current_code[j] != 0; j++) {
+                if (current_code[j] == '1')
+                    outbuf[bytepos] |= 128u >> bitpos;
 
-            bitpos++;
-            if (bitpos > 7) {
-                bytepos++;
-                bitpos = 0;
-                outbuf[bytepos] = 0;
+                bitpos++;
+
+                if (bitpos > 7) {
+                    bytepos++;
+
+                    bitpos = 0;
+                    outbuf[bytepos] = 0;
+                }
             }
         }
     }
     fwrite(outbuf, 1, bytepos + (bitpos > 0 ? 1 : 0), output);
 }
 
-void code(FILE *input ,FILE *output) {
-    int letters_count[256] = {0};
-    size_t text_len = make_signs(input, letters_count);
-    fseek(input, 2, SEEK_SET);
-
-    Tnode *root = NULL;
-    root = make_huff_tree(letters_count);
-
-    unsigned char *outbuf = (unsigned char *) malloc(SIZE_OUT);
-    memset(outbuf, 0, SIZE_OUT);
-    outbuf[0] = (unsigned char) (text_len >> 24);
-    outbuf[1] = (unsigned char) (text_len >> 16);
-    outbuf[2] = (unsigned char) (text_len >> 8);
-    outbuf[3] = (unsigned char) text_len;
-    unsigned int bitpos = 32;
-
-    unsigned char *codesTable[256] = {NULL};
-
-    unsigned char path[256] = {'\0'};
-
-    if (text_len > 0)
-        save_huffman_tree(root, &bitpos, outbuf);
-
-    make_huff_table(root, codesTable, path, 0);
-
-    write_encode(input, output, &bitpos, outbuf, codesTable);
-    free(outbuf);
-    delete_tree(root);
+void make_huff_table(unsigned char path[256], unsigned int level, unsigned char *code[256], Tnode *root) {
+    if (root->left || root->right) //if !leaf
+    {
+        path[level] = '0';
+        make_huff_table(path, level + 1, code, root->left);
+        path[level] = '1';
+        make_huff_table(path, level + 1, code, root->right);
+    } else {
+        path[level] = 0;
+        code[root->symbol] = memcpy(malloc(level + 1), path, level + 1);
+    }
 }
 
-Tnode *read_huffman_tree(unsigned char *outbuf, size_t *total_bitpos) {
-    Tnode *new = (Tnode *) malloc(sizeof(Tnode));
-    unsigned int bit = outbuf[*total_bitpos / 8] & (128u >> (*total_bitpos % 8));
-    (*total_bitpos)++;
-
-    if (bit) { //не лист
-
-        new->left = read_huffman_tree(outbuf, total_bitpos);
-        new->right = read_huffman_tree(outbuf, total_bitpos);
-    } else { //лист
-
-        unsigned int bitpos = *total_bitpos % 8;
-        new->left = NULL;
-        new->right = NULL;
+void save_tree(Tnode *root, unsigned int *total_bitpos, unsigned char *outbuf) {
+    if (root->left) {
+        /*записать_в_файл_1_бит(файл, 1)*/
+        outbuf[*total_bitpos / 8] |= 128u >> ((*total_bitpos) % 8);
+        *total_bitpos += 1;
+        save_tree(root->left, total_bitpos, outbuf);
+        save_tree(root->right, total_bitpos, outbuf);
+    } else {
+        unsigned int bitpos;
+        /*записать_в_файл_1_бит(файл, 0)*/
+        *total_bitpos += 1;
+        bitpos = (*total_bitpos) % 8;
+        /*записать_в_файл_8_бит(файл, root->symbol)*/
         if (bitpos == 0) {
-            new->value = outbuf[*total_bitpos / 8];
+            outbuf[*total_bitpos / 8] = root->symbol;
         } else {
-            new->value = (unsigned char) (outbuf[*total_bitpos / 8] << bitpos);
-            new->value |= outbuf[*total_bitpos / 8 + 1] >> (8 - bitpos);
+            outbuf[*total_bitpos / 8] |= root->symbol >> bitpos;
+            outbuf[*total_bitpos / 8 + 1] |= root->symbol << (8 - bitpos);
         }
         *total_bitpos += 8;
     }
-    return new;
 }
 
-size_t read_header(unsigned char *outbuf, size_t *bitpos, size_t message_len) {
-    message_len = outbuf[0] << 24;
-    message_len |= outbuf[1] << 16;
-    message_len |= outbuf[2] << 8;
-    message_len |= outbuf[3];
-    *bitpos = 32;
+Tnode *build_tree(unsigned int *letters) {
+    Tnode *ArrNode[256] = {NULL};
+    int count = 0;
+    for (int i = 0; i < 256; i++) {
 
-    return message_len;
-}
-
-void write_raw(unsigned char *inpbuf, size_t bitpos, Tnode *root, size_t message_len, FILE *output) {
-    unsigned char outbuf[SIZE_OUT];
-    size_t k;
-    size_t outpos = 0;
-    size_t byte_pos = bitpos / 8;
-    size_t bit_pos = bitpos % 8;
-
-    for (k = 0; k < message_len; k++) {
-        Tnode *node = root;
-
-        while ((node->left != NULL) && (root->right != NULL)) {
-            node = (inpbuf[byte_pos] & (128u >> bit_pos)) ? node->right : node->left;
-            if (7 == bit_pos++) {
-                bit_pos = 0;
-                byte_pos++;
+        if (letters[i]) {
+            ArrNode[count] = (Tnode *) malloc(sizeof(Tnode));
+            ArrNode[count]->freq = letters[i];
+            ArrNode[count]->symbol = i;
+            ArrNode[count]->left = NULL;
+            ArrNode[count]->right = NULL;
+            count++;
+        }
+    }
+    while (count > 1) {
+        for (int i = 0; i < count - 1; i++) {
+            for (int j = 0; j < count - i - 1; j++) {
+                if (ArrNode[j]->freq < ArrNode[j + 1]->freq) {
+                    Tnode *tmp = ArrNode[j];
+                    ArrNode[j] = ArrNode[j + 1];
+                    ArrNode[j + 1] = tmp;
+                }
             }
         }
-        outbuf[outpos++] = (unsigned char) node->value;
+        Tnode *NewNode = newNode(ArrNode[count - 2], ArrNode[count - 1],
+                                 ArrNode[count - 2]->freq + ArrNode[count - 1]->freq, 0);
+        ArrNode[count - 2] = NewNode;
+        count--;
     }
-    if (outpos > 0) {
-        fwrite(outbuf, 1, outpos, output);
-    }
+    Tnode *root = ArrNode[0];
+    return root;
 }
 
-void decode(FILE *input ,FILE *output) {
-    unsigned char *outbuf = (unsigned char *) malloc(SIZE_OUT);
-    size_t bitpos = 0;
-    size_t message_len = 0;
-
-    fread(outbuf, 1, SIZE_OUT, input);
-
-    message_len = read_header(outbuf, &bitpos, message_len);
-
-    Tnode *root = (message_len == 0) ? NULL : read_huffman_tree(outbuf, &bitpos);
-
-    write_raw(outbuf, bitpos, root, message_len, output);
-    free(outbuf);
-    delete_tree(root);
+void encode(FILE *input, FILE *output) {
+    unsigned int lettersFreq[256] = {0};
+    int symbol = 0;
+    unsigned int message_len = 0;
+    while ((symbol = fgetc(input)) != EOF) {
+        message_len++;
+        lettersFreq[symbol]++;
+    }
+    if (message_len == 0)
+        exit(0);
+    Tnode *root = build_tree(lettersFreq);
+    //Finished build Huffman Tree
+    unsigned char *outbuf = (unsigned char *) malloc(OUTBUF_SIZE);
+    memset(outbuf, 0, OUTBUF_SIZE);
+    outbuf[0] = (unsigned char) (message_len >> 24);
+    outbuf[1] = (unsigned char) (message_len >> 16);
+    outbuf[2] = (unsigned char) (message_len >> 8);
+    outbuf[3] = (unsigned char) message_len;
+    unsigned int bitpos = 32;
+    /*сохранить дерево*/
+    save_tree(root, &bitpos, outbuf);
+    unsigned char *codesTable[256] = {0},
+            path[256] = {0};
+    make_huff_table(path, 0, codesTable, root);
+    fseek(input, 3, SEEK_SET);
+    decoder(input, output, &bitpos, outbuf, codesTable);
 }
