e0f3d6870a7ab508647c421742ff7a15d3ba256e
Stanislav <62795541+NIdIN001@users.noreply.github.com>
2020-06-28 23:52:58 +0700

    Update header.h

--- 9.0 Dijkstra/header.h
+++ 9.0 Dijkstra/header.h
@@ -1,12 +1,11 @@
-//
-// Created by Станислав Уточкин on 27.04.2020.
-//
-
 #ifndef LABA9_HEADER_H
 #define LABA9_HEADER_H
+
 #include <stdio.h>
 #include <limits.h>
 #include <string.h>
+#include <stdlib.h>
 
-void dijkstra(int count_vertex, int from, unsigned int edges[][count_vertex],unsigned int* d, int* result);
+void dijkstra(int count_vertex, int from, unsigned int** edges,unsigned int* d, int* result);
+
 #endif //LABA9_HEADER_H
