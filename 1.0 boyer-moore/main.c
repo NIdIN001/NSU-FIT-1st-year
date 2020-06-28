edd601cdb185cf72fb296626f40e35445622fc76
Stanislav <62795541+NIdIN001@users.noreply.github.com>
2020-06-28 23:39:40 +0700

    Update main.c

--- 1.0 boyer-moore/main.c
+++ 1.0 boyer-moore/main.c
@@ -1,30 +1,82 @@
 #include <stdio.h>
-#include <locale.h>
+#include <stdlib.h>
 #include <string.h>
-#include "header.h"
 
-int main(int argc, char *argv[]) {
-    setlocale(0,"");
+#define K 4
+#define SIZE (2 << K)
 
-    char example[16];
-    gets(example);
+struct RingBuffer {
+    unsigned int write_count;
+    unsigned int mask; // mask = 2^k - 1
+    unsigned char data[SIZE];
+};
 
-    //   const char *example;
-    //   example = argv[1];
+static void write(struct RingBuffer *buff, const char *value, int len_value) {
+    for (int i = 0; i < len_value; ++i) {
+        buff->data[buff->write_count & buff->mask] = value[i];
+        ++buff->write_count;
+    }
+}
 
-    size_t ex_size = strlen(example);
+static int get_elem(const struct RingBuffer *buff, unsigned int i) {
+    return buff->data[i & buff->mask];
+}
 
-    int ascii[256];
-    table_(ex_size, example, ascii);
+static void create_buf (struct RingBuffer *buff) {
+    buff->write_count = 0;
+    buff->mask = SIZE - 1;
+}
 
-    char text[100];
-    gets(text);
+int main() {
+    unsigned char pattern[17];
+    if(scanf("%16[^\n]%*c", pattern) == 0)
+        exit(EXIT_FAILURE);
+    size_t len = strlen((char*)pattern);
 
-    //   const char *text;
-    //   text = argv[2];
+    unsigned int *shift_table = malloc(sizeof(unsigned int) * 256); // table of shifts
+    if(shift_table == NULL)
+        exit(EXIT_FAILURE);
 
-    if ((strlen(text) == 0) || (strlen(example) == 0))
-        return 0;
+    for (int i = 0; i < 256; ++i) {
+        shift_table[i] = (unsigned int)len;
+    }
+    // table shifts with negative number
+    for (unsigned int i = 0; i + 1 < len; ++i) {
+        shift_table[pattern[i]] = (unsigned int)len - i - 1; // constructing table of shifts
+    }
 
-    moore_moore(ascii, ex_size, example, text);
+    struct RingBuffer buffer;
+    create_buf(&buffer);
+
+    unsigned int pos =
+            (unsigned int)len - 1; // position of pattern`s last symbol in text
+
+    char symbols[17]; // part text
+
+    size_t read_symbols_count = fread(
+            symbols, sizeof(char), (size_t)(pos - buffer.write_count) + 1, stdin);
+    write(&buffer, symbols, (int)read_symbols_count);
+
+    while (read_symbols_count != 0 && pos < buffer.write_count) {
+        unsigned int concurrences = 0;
+        while (len >= concurrences + 1 && get_elem(&buffer, pos - concurrences) ==
+                                          pattern[len - concurrences - 1]) {
+            printf("%u ", pos - concurrences + 1);
+            ++concurrences;
+        }
+        if (concurrences != len) {
+            printf("%u ", pos - concurrences + 1);
+        }
+
+        pos += shift_table[get_elem(&buffer, pos)];
+
+        if (pos >= buffer.write_count) {
+            read_symbols_count = fread(symbols, sizeof(char),
+                                       (size_t)(pos - buffer.write_count) + 1, stdin);
+            write(&buffer, symbols, (int)read_symbols_count);
+        }
+    }
+    printf("\n");
+    free(shift_table);
+    return 0;
 }
